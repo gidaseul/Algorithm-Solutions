28e375518d930631576539e7bc8f3df8824fb7be
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-02-05 14:54:16 +0900

    [D3] Title: 상호의 배틀필드, Time: 109 ms, Memory: 28,888 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,152 +1,159 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-/**
- *	SW Expert Academy
- * 	@author triplenieun
+/*
+ * SW Expert Academy 
+ * @author Da_seul
+ * 
+ * 	@see #main(String[])
+ * 		
+ * 
+ * 		문자		의미
+		.	평지(전차가 들어갈 수 있다.)
+		*	벽돌로 만들어진 벽
+		#	강철로 만들어진 벽
+		-	물(전차는 들어갈 수 없다.)
+		^	위쪽을 바라보는 전차(아래는 평지이다.)
+		v	아래쪽을 바라보는 전차(아래는 평지이다.)
+		<	왼쪽을 바라보는 전차(아래는 평지이다.)
+		>	오른쪽을 바라보는 전차(아래는 평지이다.)
+ *	
+ *		<문자 동작>
+ *		Up : 방향 up / if(한칸 위 == 평지) -> 이동
+ * 		Down : 방향 아래 / if(한칸 아래 == 평지) -> 이동
+ * 		Left : 방향 왼쪽 / if(한칸 왼쪽 == 평지) -> 이동
+ * 		Right : 방향 오른쪽 / if(한칸 오른쪽 == 평지) -> 이동
+ *		Shoot : 방향으로 포탄 발사
  *		
- *		@see #main(String[])
- * 		1. 테스트 케이스 개수를 입력 받는다.
- * 
- * 		@see #inputTestCase()
- * 		2. 각 테스트 케이스마다,
- * 			2-1. 격자판의 높이H, 너비W를 입력 받는다.
- * 				2-1-1. 입력 받을 때 ^ v < > 이 오면 방향을 저장한다.
- * 				2-1-2. H * W 격자판을 생성한다.
- * 			2-2. 명령 개수 N을 입력 받는다.
- * 			2-3. N 길이의 명령 문자열을 입력 받는다.
- *  
- * 		@see #executeCommand()
- * 		3. 다음 기능들을 수행한다.
- * 			3-1. U D L R 명령
- * 				3-1-1. 해당 방향으로 먼저 설정한다.
- * 				3-1-2. 앞으로 가기 전 장애물 혹은 격자판 밖인지 확인한다.
- * 				3-1-3. 이상이 없다면 앞으로 이동한다.
- * 			3-2. S 명령
- * 				3-2-1. 전차 방향으로 포탄이 이동할 때, (반복문)
- * 					3-2-1-1. * 벽돌 벽이 있다면 *를 .으로 변경하고 break.
- * 					3-2-1-2. # 강철 벽이 있다면 바로 break.
- * 
+ *
+ *		규칙
+ *		전차 이동: 이동 할 때 맵 밖이라면 전차는 움직일 수 없음.
+ *		포탄 발사 : 강철 벽 or 게임 맵 밖 아무일도 일어나지 않음 / 벽돌 벽은 삭제되고 평지로 개선됨.이후 강철 벽 or 맵 밖까지 멈추기)
+ *
+ *
+ *	문제 해결 접근 방식
+ *	1. 첫번째 테스트 케이스의 수 T
+ *	2. H(맵의 높이),W(너비) 공백으로 구분되어 주어짐.
+ *	3. map의 명령어에 따라 진행함.
+ *	4. 명령어 기준으로 현재 위취의 전차 방향 기준으로 명령 진행함. -> commands 
+ *	5. commands 함수를 통해 진행할 때  S 명령어 진행 시 shoot 함수 진행.
+ *
  */
-public class Solution {
-	static final int MAX_LEN = 100;
+public class Solution
+{
+	
 	static BufferedReader br;
 	static StringTokenizer st;
-	static StringBuilder sb;
-
-	static char[][] battleField;
-	static int height, width, commandNum;
-	static String commandStr;
-	static int directionIdx;	// 0:^ 1:v 2:< 3:>
-	static Map<Character, Integer> dirToIdx = new HashMap<Character, Integer>() {{
-	    put('^', 0);
-	    put('v', 1);
-	    put('<', 2);
-	    put('>', 3);
-	}};
-	static char[] idxToDir = {'^', 'v', '<', '>'};
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};
-	static int currentR, currentC;
-	private static void inputTestCase() throws Exception {
-		st = new StringTokenizer(br.readLine().trim());
-		height = Integer.parseInt(st.nextToken());
-		width = Integer.parseInt(st.nextToken());
-		
-		battleField = new char[height][width];
-		for (int rowIdx = 0; rowIdx < height; rowIdx++) {
-			String rowLine = br.readLine().trim();
-			for (int colIdx = 0; colIdx < width; colIdx++) {
-				battleField[rowIdx][colIdx] = rowLine.charAt(colIdx);
-				if (battleField[rowIdx][colIdx] == '^' 
-						|| battleField[rowIdx][colIdx] == 'v' 
-						|| battleField[rowIdx][colIdx] == '<'
-						|| battleField[rowIdx][colIdx] == '>') {
-					directionIdx = dirToIdx.get(battleField[rowIdx][colIdx]);
-					currentR = rowIdx;
-					currentC = colIdx;
-				} 
-			}
-		}
-		
-		commandNum = Integer.parseInt(br.readLine().trim());
-		commandStr = br.readLine().trim();
-	}
+	static StringBuilder sb; 
+	static char[][] map;
+	static char vector;
+	static int targetI, targetJ;	
 	
-	public static void executeCommand() {
-		for (int commandIdx = 0; commandIdx < commandNum; commandIdx++) {
-			if (commandStr.charAt(commandIdx) == 'S') {
-				shoot();
-			} else if (commandStr.charAt(commandIdx) == 'U') {
-				directionIdx = 0;
-				move();
-			} else if (commandStr.charAt(commandIdx) == 'D') {
-				directionIdx = 1;
-				move();
-			} else if (commandStr.charAt(commandIdx) == 'L') {
-				directionIdx = 2;
-				move();
-			} else if (commandStr.charAt(commandIdx) == 'R') {
-				directionIdx = 3;
-				move();
-			}
-		}
-	}
+	public static void main(String args[]) throws IOException{
 	
-	public static void shoot() {
-		int cannonBallR = currentR;
-		int cannonBallC = currentC;
 		
-		while (true) {
-			cannonBallR += dr[directionIdx];	// 이동한 대포알의 행 위치
-			cannonBallC += dc[directionIdx];	// 이동한 대포알의 열 위치
-			if (cannonBallR < 0 || cannonBallR > height - 1 || cannonBallC < 0 || cannonBallC > width - 1) break;	// 격자판 바깥
-			
-			if (battleField[cannonBallR][cannonBallC] == '*') {
-				battleField[cannonBallR][cannonBallC] = '.';
-				break;
-			} else if (battleField[cannonBallR][cannonBallC] == '#') {
-				break;
-			}
-		}
-	}
-	
-	public static void move() {
-		battleField[currentR][currentC] = idxToDir[directionIdx]; // 방향 전환
-		
-		int moveR = currentR + dr[directionIdx];
-		int moveC = currentC + dc[directionIdx];
-		if (moveR < 0 || moveR > height - 1 || moveC < 0 || moveC > width - 1) return;	// 격자판 바깥
-		if (battleField[moveR][moveC] == '.') {	// 물 또는 벽에 들어갈 수 없음
-			battleField[currentR][currentC] = '.';
-
-			currentR = moveR;
-			currentC = moveC;
-			
-			battleField[currentR][currentC] = idxToDir[directionIdx];
-		}
-	}
-	
-	static int testCaseNum;
-	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		testCaseNum = Integer.parseInt(br.readLine().trim());
-		for (int testCaseIdx = 1; testCaseIdx <= testCaseNum; testCaseIdx++) {
-			inputTestCase();
-			executeCommand();
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int H = Integer.parseInt(st.nextToken());
+			int W = Integer.parseInt(st.nextToken());
 			
-			sb.append("#" + testCaseIdx + " ");
-			for (int rowIdx = 0; rowIdx < height; rowIdx++) {
-				for (int colIdx = 0; colIdx < width; colIdx++) {
-					sb.append(battleField[rowIdx][colIdx]);
-				} 
+					
+			map = new char[H][W];
+						
+			// 문자열 배열 값 저장
+			for(int i =0; i<H;i++) {
+				String s = br.readLine(); 
+				for(int j=0; j<W; j++) {
+					if(s.charAt(j) == '>' || s.charAt(j) == '<' ||s.charAt(j) == '^' ||s.charAt(j) == 'v') {
+						targetI = i;
+						targetJ = j;
+					}
+					map[i][j] = s.charAt(j);
+				}
+			}
+			
+			// 사용자 문자 동작 인식 공격
+			int N = Integer.parseInt(br.readLine());
+			String s = br.readLine();
+			for (int i=0; i<N; i++) {
+				commands(s.charAt(i));
+			}
+			sb = new StringBuilder();
+			sb.append('#').append(t).append(" ");
+			for(int i=0; i<H; i++) {
+				for(int j=0; j<W; j++) {
+					sb.append(map[i][j]);
+				}
 				sb.append("\n");
 			}
+			System.out.print(sb);
 		}
-		System.out.println(sb.toString());
+	}
+	
+	static void commands(char cmd) {
+		int r = targetI;
+		int c = targetJ;
+		
+		if (cmd == 'U') {
+			if (r-1 >= 0 && map[r-1][c] == '.') {
+				map[r][c] = '.';
+				map[r-1][c] = '^';
+				targetI = r-1;
+			}else map[r][c] = '^';
+		}
+		
+		else if (cmd == 'D') {
+			if (r+1 < map.length && map[r+1][c] == '.') {
+				map[r][c] = '.';
+				map[r+1][c] = 'v';  
+				targetI = r+1;
+			}else map[r][c] = 'v';
+		}
+		
+		else if (cmd == 'L') {
+			if(c-1 >= 0 && map[r][c-1] == '.') {
+				map[r][c] = '.';
+				map[r][c-1] ='<';
+				targetJ = c-1;
+			}else map[r][c] = '<';
+		}
+		else if (cmd == 'R'){
+			if(c+1 <map[0].length && map[r][c+1] == '.') {
+				map[r][c] = '.';
+				map[r][c+1] ='>';
+				targetJ = c+1;
+			}else map[r][c] = '>';
+		}
+		else if (cmd == 'S'){
+			vector = map[r][c];
+			shoot(r,c);
+			}
+		}
+
+	static void shoot(int r, int c) {
+	    int dr = 0, dc = 0;
+	    int H = map.length;
+	    int W = map[0].length;
+	    
+	    if (map[r][c] == '^') dr = -1;
+	    else if (map[r][c] == 'v') dr = 1;
+	    else if (map[r][c] == '<') dc = -1;
+	    else if (map[r][c] == '>') dc = 1;
+
+	    int nr = r + dr;
+	    int nc = c + dc;
+
+	    while (nr >= 0 && nr < H && nc >= 0 && nc < W) {
+	        if (map[nr][nc] == '*') {
+	            map[nr][nc] = '.';
+	            break;
+	        }
+	        if (map[nr][nc] == '#') break;
+
+	        nr += dr;
+	        nc += dc;
+	    }
 	}
 }
