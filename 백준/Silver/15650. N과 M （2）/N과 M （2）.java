8e676ff8efcf1c308dc8010a2957776cd4b90860
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-02-05 17:35:07 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,52 +1,51 @@
-import java.util.*;
-import java.io.*;
-import java.lang.management.ManagementFactory;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-/*
- * SW Expert Academy 
- * @author Da_seul
- * 
- * 	@see #main(String[])
- * 		
+/**
+ *	BOJ
+ * 	@author triplenieun
+ *		
+ *	1. N과 M을 입력 받는다.
+ *	
+ *	2. 조합 함수를 구성하여 nCm의 모든 경우를 출력한다.
+ *
+ *
  */
-public class Main
-{
-	
+public class Main {
 	static BufferedReader br;
 	static StringTokenizer st;
 	static StringBuilder sb;
-	static void comb(int start, int n, int r, List<Integer> path) {
-		
-		
-		// 가지치기 - 현재 내가 가지고 있는 숫자와 남은 숫자들을 더해도 r을 만들지 못할 경우 안됨.
-		if (path.size() + (n-start+1)<r) {
-			return;
-		}
-		
-		// 종료 조건
-		if(path.size() == r) {
-			sb = new StringBuilder();
-			for (int x : path) {
-				sb.append(x).append(" ");
+	
+	static int[] numbers;
+	public static void combination(int cnt, int start) {
+		if (cnt == M) {
+			for (int number : numbers) {
+				sb.append(number).append(" ");
 			}
-			System.out.println(sb.toString().trim());
+			sb.append("\n");
 			return;
 		}
-		
-		// 다음 숫자 선택
-		for(int i =start; i<=n; i++) {
-			path.add(i); // i 선택
-			comb(i+1,n,r,path); // 다음 단계
-			path.remove(path.size()-1); // 선택 취소
+		for (int combIdx = start; combIdx <= N; combIdx++) {
+			numbers[cnt] = combIdx;
+			combination(cnt + 1, combIdx + 1);
 		}
 	}
-	public static void main(String args[]) throws IOException
-	{
+	
+	static int N;
+	static int M;
+	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		sb = new StringBuilder();
+		st = new StringTokenizer(br.readLine().trim());
 		
-		comb(1,N,M,new ArrayList<>());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int cnt = 0;
+		numbers = new int[M];
+		combination(cnt, 1);
+		
+		System.out.println(sb.toString());
 	}
 }
