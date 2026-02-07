05ce0179c2a5e349194298988570b8af3a57a7b1
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-02-07 15:27:41 +0900

    Title: 정수의 조건 여부 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260207/정수의 조건 여부 3/numbers-condition-3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        
+        if ((A % 13) == 0 || (A%19) ==0){
+            System.out.println("True");
+        }else {System.out.println("Flase");
+                }
+
+    }
+}
