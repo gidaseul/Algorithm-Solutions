80c106d7c7ff31263d5dc3e95f68206d752c469f
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-02-24 14:37:13 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 114 ms, Memory: 30,464 KB -BaekjoonHub

---
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * SW Expert Academy 1227 : [문제 해결 기본] 7일차 - 미로2
+ *
+ *  문제 설명:
+ *      1. 100 x 100 미로 존재 (공간 배열의 100 x 100) 이라는 점을 잘 생각해봐야 함.
+ *      2. 시작점 (1,1) : '2' -> 도착점 (13,13) : '3'
+ *      3. 출발점 부터 도착지점까지 갈 수 있는 길이 있는지 여부 파악하기
+ *
+ *  해결 접근:
+ *      1. 미로를 탐색하는 부분에서 백트래킹 방법으로 미로를 풀 수 있을 것 같음.
+ *          1-1. (1,1) 에서 탐색 시작함.
+ *          1-2. 탐색 방향은 4가지 방향 존재함.
+ *              1-2-1. 방향을 탐색하면서 주의할 점.
+ *                  1-2-1-1. 방해물(1), 이미 갔던 곳, 배열을 벗어난 nx,ny 가 아닐 경우메나 탐색
+ *              1-2-2. 탐색 하면서 boolean 의 형태로 값을 반환 받을 때 dfs의 호출 값도 true로 반환 값을 전달해야 함.
+ *
+ *
+ */
+public class Solution {
+
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    static boolean[][] check;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int[][] map;
+
+    public static void main(String[] args) throws Exception {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        for (int tc = 1; tc <= 10; tc++) {
+            map= new int[100][100];
+            check = new boolean[100][100];
+            String T = br.readLine();
+            for (int i = 0; i < 100; i++) {
+                String line = br.readLine().trim();
+                for (int j = 0; j < 100; j++) {
+                    int a = line.charAt(j) - '0';
+                    map[i][j] = a;
+                }
+            }
+            sb.append("#").append(tc).append(" ");
+            if(dfs(1,1)) sb.append(1).append("\n");
+            else sb.append(0).append("\n");
+        }
+        System.out.println(sb);
+        sb.setLength(0);
+    }
+
+    static boolean dfs(int x, int y){
+        if(map[x][y] == 3){
+            return true;
+        }
+        for(int i=0; i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+
+            if(nx >=0 && nx < 100 && ny >= 0 && ny < 100 && map[nx][ny] !=1){
+                if(!check[nx][ny]){
+                    check[nx][ny] = true;
+                    if(dfs(nx,ny)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
