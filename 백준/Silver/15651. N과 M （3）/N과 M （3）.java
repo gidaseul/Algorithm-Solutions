f6d12a82fcbba2ad2d801ed7f4452d930e9d66f5
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-03-08 02:02:52 +0900

    [Silver III] Title: N과 M (3), Time: 332 ms, Memory: 112056 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    static int N,M;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine().trim());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        result = new int[M];
+        perm(0);
+        System.out.print(sb);
+
+    }
+    static void perm(int depth){
+
+        if(depth == M){
+            for(int x : result){
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            result[depth] = i+1;
+            perm(depth+1);
+        }
+    }
+
+}
