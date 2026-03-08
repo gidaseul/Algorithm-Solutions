62a84df128044827e1e1f38a09278a1a986567ca
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-03-08 16:12:48 +0900

    Title: 반복 출력하기 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260308/반복 출력하기 2/repeated-output-2.java
+++ 260308/반복 출력하기 2/repeated-output-2.java
@@ -8,10 +8,10 @@ public class Main {
     }
     static void print(int n){
 
-        if(n==0{
+        if(n==0){
             return;
-        })
-        
+        }
+
         print(n-1);
         System.out.println("HelloWorld");
 
