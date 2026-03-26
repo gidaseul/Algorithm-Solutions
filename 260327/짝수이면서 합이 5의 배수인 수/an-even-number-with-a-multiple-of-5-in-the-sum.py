7c8bd6dda5416da4db453bb9d032bf6978db91d8
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-03-27 01:05:38 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260327/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
+++ 260327/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
@@ -5,7 +5,7 @@ def check(n):
     ten = n/10
     one = n%10
     result = ten+one
-    if(n % 2 == 0 && result % 5 ==0)
+    if(n % 2 == 0 and result % 5 ==0)
         return "Yes"
     else:
         return "No"
