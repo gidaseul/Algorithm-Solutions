e4be648ee98c3a658139007b3c30dec1631717e3
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-04 18:14:05 +0900

    Title: 함수를 이용한 소수 판별, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260404/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,19 @@
+
+a, b = tuple(map(int, input().split()))
+
+# Please write your code here.
+def isPrime(n):
+    if n == 1:
+        return False
+    
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+sum_val = 0
+for i in range(a,b+1):
+    if isPrime(i):
+        sum_val += i
+
+print(sum_val)
