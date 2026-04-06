badb4dc910c71e83ac8be79995f3229f729d3e07
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-06 19:51:55 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 17056 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int[] arr = new int[1000001];
+        
+        for (int i=2; i<=a; i++) {
+        	arr[i] = arr[i-1] + 1;
+        	
+        	if (i%2==0) {
+        		arr[i] = Math.min(arr[i], arr[i/2]+1);
+        	}
+        	
+        	if (i%3==0) {
+        		arr[i] = Math.min(arr[i], arr[i/3]+1);
+        	}
+        }
+       
+      
+        System.out.println(arr[a]);
+    }
+}
