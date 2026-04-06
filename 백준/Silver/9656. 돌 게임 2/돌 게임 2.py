557aa78855ae859cbdd56f91f340d9d3fd23e240
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-07 01:17:35 +0900

    [Silver V] Title: 돌 게임 2, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -0,0 +1,18 @@
+import math
+
+n = int(input())
+
+
+dp = [0] * (1000+1)
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 1
+
+for i in range(4,n+1):
+    dp[i] = min(dp[i-1],dp[i-3]) +1
+
+if dp[n] % 2==1:
+    print("CY")
+else:
+    print("SK")
