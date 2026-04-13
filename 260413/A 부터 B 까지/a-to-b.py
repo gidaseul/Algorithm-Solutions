3b2981cdf0030f7975629acdf06aed8b84effb8d
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-13 16:59:38 +0900

    Title: A 부터 B 까지, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/A 부터 B 까지/a-to-b.py
+++ 260413/A 부터 B 까지/a-to-b.py
@@ -1,9 +1,11 @@
 A,B = map(int,input().split())
-arr = []
-for i in range(A,B+1):
-    if i %2==0:
-        arr.append(i*3)
-    else:
-        arr.append(i*2)
 
-print(*arr)
+nums = []
+
+while A <= B:
+    nums.append(A)
+    if A % 2==0:
+        A+=3
+    else:
+        A*=2
+print(*nums)
