06bd61ef16857d26a12911fca805ca9a7e033f62
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-13 18:02:40 +0900

    Title: 두 배열의 곱, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260413/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,16 @@
+arr1 = []
+arr2 = []
+
+for _ in range(3):
+    row = list(map(int, input().split()))
+    arr1.append(row)
+
+for _ in range(3):
+    row = list(map(int, input().split()))
+    arr2.append(row)
+
+# 같은 위치 원소끼리 곱하기
+result = [[arr1[i][j] * arr2[i][j] for j in range(3)] for i in range(3)]
+
+for row in result:
+    print(*row)
