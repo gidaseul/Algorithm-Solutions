5f210c0c735b0c6e41bfa4d305af1d43794e1ffb
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-13 17:04:39 +0900

    Title: 특정 수 맞히기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260413/특정 수 맞히기/catching-specific-number.py
@@ -0,0 +1,11 @@
+while True:
+
+    num = int(input())
+    if num == 25:
+        print("Good")
+        break
+    elif num < 25:
+        print("Higher")
+    elif num >25:
+        print("Lower")
+    
