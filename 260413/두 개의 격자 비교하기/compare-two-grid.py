79e3e702089b75d997d9b0f625bf9733fab1d9ff
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-13 18:05:40 +0900

    Title: 두 개의 격자 비교하기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260413/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+arr1 = []
+for _ in range(N):
+    row = list(map(int, input().split()))
+    arr1.append(row)
+
+input()  # 빈 줄 건너뛰기
+
+arr2 = []
+for _ in range(N):
+    row = list(map(int, input().split()))
+    arr2.append(row)
+
+result = [[0 if arr1[i][j] == arr2[i][j] else 1 for j in range(M)] for i in range(N)]
+
+for row in result:
+    print(*row)
