7c21c478cf64eac468a47502a81746b555f93822
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-16 00:24:05 +0900

    Title: 행복한 수열의 개수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260416/행복한 수열의 개수/number-of-happy-sequence.py
+++ 260416/행복한 수열의 개수/number-of-happy-sequence.py
@@ -7,26 +7,44 @@ for i in range(n):
     
     # 가로 열 마다 m을 넘는지
     cnt = 0
-    for j in range(n-1):
-
-        if grid[i][j] == grid[i][j+1]:
-            cnt +=1
-        else:
+    for j in range(n):
+        if j == 0:
+            temp = grid[i][j]
             cnt = 1
-        
-        if cnt >= m:
-            count+=1
-            break
+            if cnt >=m:
+                count+=1
+                break
+        else:
+            if temp == grid[i][j]:
+                cnt+=1
+            else:
+                temp = grid[i][j]
+                cnt =1
+
+            if cnt>=m:
+                count+=1
+                break
+
 
     cntK = 0
-    for k in range(n-1):
-        if grid[k][i] == grid[k+1][i]:
-            cntK+=1
-        else:
+    for k in range(n):
+        if j == 0:
+            temp = grid[k][i]
             cntK = 1
-        if cntK>=m:
-            count+=1
-            break
+            if cntK >=m:
+                count+=1
+                break
+        else:
+            if temp == grid[k][i]:
+                cntK+=1
+            else:
+                temp = grid[k][i]
+                cntK =1
+
+            if cntK>=m:
+                count+=1
+                break
+
 print(count)
 
     
