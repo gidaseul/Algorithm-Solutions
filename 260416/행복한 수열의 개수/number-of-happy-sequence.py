83747b8005d9fccd3d1e156a01776f2a5bd5ad05
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-16 00:27:21 +0900

    Title: 행복한 수열의 개수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260416/행복한 수열의 개수/number-of-happy-sequence.py
+++ 260416/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,50 +1,29 @@
+def is_happy(line, m):
+    cnt = 1
+    for i in range(1, len(line)):
+        if line[i] == line[i - 1]:
+            cnt += 1
+        else:
+            cnt = 1
+
+        if cnt >= m:
+            return True
+
+    return m == 1
+
+
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 count = 0
 
 for i in range(n):
-    
-    # 가로 열 마다 m을 넘는지
-    cnt = 0
-    for j in range(n):
-        if j == 0:
-            temp = grid[i][j]
-            cnt = 1
-            if cnt >=m:
-                count+=1
-                break
-        else:
-            if temp == grid[i][j]:
-                cnt+=1
-            else:
-                temp = grid[i][j]
-                cnt =1
+    row = grid[i]
+    col = [grid[j][i] for j in range(n)]
 
-            if cnt>=m:
-                count+=1
-                break
-
-
-    cntK = 0
-    for k in range(n):
-        if k == 0:
-            temp = grid[k][i]
-            cntK = 1
-            if cntK >=m:
-                count+=1
-                break
-        else:
-            if temp == grid[k][i]:
-                cntK+=1
-            else:
-                temp = grid[k][i]
-                cntK =1
-
-            if cntK>=m:
-                count+=1
-                break
+    if is_happy(row, m):
+        count += 1
+    if is_happy(col, m):
+        count += 1
 
 print(count)
-
-    
