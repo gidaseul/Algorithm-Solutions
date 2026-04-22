833d1b8306e00b3068b31c55c91d17d7937e1a8d
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-23 00:17:16 +0900

    Title: 정수 명령 처리 5, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/정수 명령 처리 5/process-numeric-commands-5.py
+++ 260423/정수 명령 처리 5/process-numeric-commands-5.py
@@ -8,9 +8,8 @@ for _ in range(N):
     command.append(line[0])
     if line[0] == "push_back":
         num.append(int(line[1]))
-        print(int(line[1]))
     elif line[0] == "get":
-        n = int(line[1])
+        n = int(line[1])-1
         print(num[n])
     elif line[0] == "size":
         print(len(num))
