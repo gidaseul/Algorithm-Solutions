3641b5c8b01ec7876f1e5b944a10125051a0a7b6
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-25 18:22:11 +0900

    Title: Factorial, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260425/Factorial/factorial.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+# Please write your code here.
+
+def factorial(N):
+    if N == 0 or N== 1:
+        return 1
+
+    if N == 2:
+        return 2
+
+    sum = 0
+    for i in range(1,N+1):
+        sum *= i
+    
+    return sum
+
+print(factorial(N))
