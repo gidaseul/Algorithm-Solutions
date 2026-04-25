f6b8fb557b8d6355e7ddc1a4340965250ae7a5cb
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-26 01:39:38 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260426/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 260426/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -2,5 +2,10 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-arr.sort()
-print(arr[-1])
+def func(a):
+    if a==0:
+        return arr[0]
+    
+    return max(func(a-1),arr[a])
+
+print(func(n-1))
