eee2cf0916a0fdc7382efcdef28fa0c6e8eb51d0
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-27 18:44:58 +0900

    Title: 중앙값 계산 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/중앙값 계산 2/get-median-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+arr.sort()
+
+num = n//2
+result = []
+for i in range(num):
+    result.append(arr[i])
+
+print(*result)
