3501275e19e438cd116cb1fbee54f21ddf667cd7
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-28 00:44:46 +0900

    Title: Date to Date, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/Date to Date/date-to-date.py
+++ 260428/Date to Date/date-to-date.py
@@ -6,21 +6,12 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 length = m2-m1
-result = num_of_days[m1] - d1+1
-check = True
 
-for i in range(m1+1,m2):
-    if length == 0:
-        check = False
-        break
-    
-    else:
+if length == 0:
+    print(d2 - d1 + 1)
+else:
+    result = num_of_days[m1] - d1 + 1
+    for i in range(m1 + 1, m2):
         result += num_of_days[i]
-
-
-if check:
     result += d2
     print(result)
-else:
-    print(d2-d1+1)
-
