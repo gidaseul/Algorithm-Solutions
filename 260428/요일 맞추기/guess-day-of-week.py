fd8c57aa31ad4e086f1edb12e918bebdb6164f72
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-28 01:11:41 +0900

    Title: 요일 맞추기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/요일 맞추기/guess-day-of-week.py
+++ 260428/요일 맞추기/guess-day-of-week.py
@@ -2,20 +2,21 @@ import math
 
 m1, d1, m2, d2 = map(int, input().split())
 
+def to_days(m, d):
+    return sum(num_of_days[1:m]) + d  # 1월부터 m-1월까지 합산 + d
+
 #                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_of_the_week = ["Mon", "Tue", "Wed","Thu","Fri","Sat","Sun"]
 
 
-num1 = num_of_days[m1] + d1
+num1 = to_days(m1, d1)
+num2 = to_days(m2, d2)
 
-num2 = num_of_days[m2] + d2
+diff = num2- num1
 
-idx = 0
-if num2-num1 <0:
-    idx = abs(num2-num1) % 7
-    print(day_of_the_week[-idx])
-
+idx = diff % 7
+if diff>=0:
+    print(day_of_the_week[idx])
 else:
-    idx = (num2-num1) % 7    
     print(day_of_the_week[idx])
