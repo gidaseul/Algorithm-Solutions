8df5be2bee17150ab4466ce5d822d3366f60b13e
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-27 19:01:23 +0900

    Title: 중앙값 계산 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/중앙값 계산 2/get-median-2.py
+++ 260427/중앙값 계산 2/get-median-2.py
@@ -2,11 +2,18 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-arr.sort()
 
-num = n//2
-result = []
-for i in range(num+1):
-    result.append(arr[i])
+result = [arr[0]]
 
+for i in range(3,n+1):
+    if i%2 != 0:
+        sum = []
+        for j in range(i):
+            sum.append(arr[j])
+        
+        sum.sort()
+        idx = len(sum) //2
+        result.append(sum[idx])
+    else:
+        continue
 print(*result)
