65b0bd492e9b4c0118128dd3e242cd36059ddbfe
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-28 17:04:51 +0900

    Title: 선두를 지켜라, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260428/선두를 지켜라/keep-the-lead.py
+++ 260428/선두를 지켜라/keep-the-lead.py
@@ -1,19 +1,33 @@
 n, m = map(int, input().split())
 
 # Process A's movements
-v = []
-t = []
+a1 = [0]
 for _ in range(n):
     vi, ti = map(int, input().split())
-    v.append(vi)
-    t.append(ti)
+    result = a1[-1] + vi
+    for _ in range(ti):
+        a1.append(result)
 
 # Process B's movements
-v2 = []
-t2 = []
+a2 = [0]
 for _ in range(m):
     vi, ti = map(int, input().split())
-    v2.append(vi)
-    t2.append(ti)
+    result = a2[-1]+vi
+    for _ in range(ti):
+        a2.append(result)
 
 # Please write your code here.
+cnt = 1
+check = True
+for i in range(len(a1)):
+
+    if a1[i] > a2[i] and check:
+        check = False
+        cnt+=1
+    elif a1[i] < a2[i] and not check:
+        check = True
+        cnt+=1
+
+print(cnt) 
+        
+    
