c2e6a7a20bf0cbb870b77814e5ca1e41f74905fb
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-28 15:42:00 +0900

    Title: 왔다 갔던 구역 2, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260428/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,34 @@
+# 선분을 배열로 표현
+
+OFFSET = 1000
+
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+# Please write your code here.
+
+result = [0 for _ in range(2*OFFSET+1)]
+
+pos = OFFSET
+for i in range(n):
+    if dir[i] == 'R':
+        for _ in range(x[i]):
+            pos += 1 
+            result[pos] += 1
+
+    if dir[i] == 'L':
+        for _ in range(x[i]):
+            result[pos] += 1
+            pos -= 1
+
+res = 0
+for i in range(2*OFFSET+1):
+    if result[i] >= 2:
+        res += 1
+
+print(re) 
