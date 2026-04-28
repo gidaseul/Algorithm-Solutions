27ebc930a46049186b3209aad75db7f47d4bd2d0
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-28 17:23:32 +0900

    Title: 선두를 지켜라, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260428/선두를 지켜라/keep-the-lead.py
+++ 260428/선두를 지켜라/keep-the-lead.py
@@ -5,39 +5,34 @@ a1 = [0]
 for _ in range(n):
     vi, ti = map(int, input().split())
     for _ in range(ti):
-        a1.append(a1[-1]+v1)
+        a1.append(a1[-1]+vi)
 
 # Process B's movements
 a2 = [0]
 for _ in range(m):
     vi, ti = map(int, input().split())
     for _ in range(ti):
-        a2.append(a2[-1]+v1)
+        a2.append(a2[-1]+vi)
 
 # Please write your code here.
 
-cur = "A"
+cur = "None"
 cnt = 0
 for i in range(1,len(a1)):
 
     if a1[i] > a2[i]:
         result = "A"
-        
-        if cur == result:
-            continue
-        else:
-            cur = "A"
-            cnt +=1
-
     elif a1[i] < a2[i]:
         result = "B"
-
-        if cur == result:
-            continue
-        else:
-            cur = "B"
-            cnt +=1
     else:
         continue
-        
+
+    if cur is None:       # 첫 선두 결정은 카운트 안 함
+        cur = result
+    elif cur != result:   # 선두가 바뀔 때만 카운트
+        cur = result
+        cnt += 1
+
+
+
 print(cnt)
