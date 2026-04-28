b2d61e877c5817a40630a13f3950fd59f791af0f
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-28 16:42:10 +0900

    Title: 선두를 지켜라, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260428/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+# Process A's movements
+v = []
+t = []
+for _ in range(n):
+    vi, ti = map(int, input().split())
+    v.append(vi)
+    t.append(ti)
+
+# Process B's movements
+v2 = []
+t2 = []
+for _ in range(m):
+    vi, ti = map(int, input().split())
+    v2.append(vi)
+    t2.append(ti)
+
+# Please write your code here.
