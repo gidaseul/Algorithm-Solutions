ec05b98d03286afe0a14929736fc82bcbba096a4
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-29 16:50:15 +0900

    Title: 국영수 순이지, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260429/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+class stu:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+student = []
+
+for _ in range(n):
+    n_i, k_i, e_i, m_i = input().split()
+    student.append(stu(n_i, k_i, e_i, m_i))
+# Please write your code here.
+
+student.sort(key=lambda x : (x.kor,x.eng,x.math))
+
+for i in student:
+    print(i.name, i.kor, i.eng, i.math)
