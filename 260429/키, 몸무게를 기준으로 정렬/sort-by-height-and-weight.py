2899923e9cd780c6dc9fc2a5b89528d78c2f61f2
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-04-29 21:25:06 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+class stu:
+    def __init__(self,name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+
+
+student = []
+
+for _ in range(n):
+    n, h, w = input().split()
+    student.append(stu(n , int(h), int(w)))
+# Please write your code here.
+
+student.sort(key=lambda x : (x.height, -x.weight))
+for i in student:
+    print(i.name , i.height, i.weight)
