9b9385ea34406b29f7707d222c037b056c7ebed6
gidaseul <61573303+gidaseul@users.noreply.github.com>
2026-05-12 15:58:01 +0900

    [Passed] 수들의 합 최대화하기 (127ms, 21MB)

--- 260512/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 260512/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -3,23 +3,9 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-visited = [[False] * n for _ in range(n)]
+visited = [False] * (n+1)
+
 answer = []
-
-def is_False(x,y):
-    for i in range(n):
-        for j in range(n):
-            if j==y:
-                visited[i][j] = False 
-
-def is_True(x,y):
-    for i in range(n):
-        for j in range(n):
-            if j==y:
-                visited[i][j] = True
-            else:
-                continue
-
 
 def choose(curr_num):
 
@@ -28,21 +14,19 @@ def choose(curr_num):
         return max_result
 
     max_result = 0
-    
-    for j in range(n):
-        if visited[curr_num][j]:
-                continue
-        
-            # 이때 행, 열 모두 처리해야 함. 
-        is_True(curr_num,j)
-        answer.append(grid[curr_num][j])
 
-        max_result = max(choose(curr_num+1), max_result)
-    
-        is_False(curr_num,j)
-        answer.pop()
-    return max_result
+    for i in range(n):
+        if visited[i]:
+            continue
         
+        visited[i] = True
+        answer.append(grid[curr_num][i])
+        max_result = max(max_result, choose(curr_num+1))
+
+        visited[i] = False
+        answer.pop()
+
+    return max_result
 
 result = choose(0)
 print(result)
